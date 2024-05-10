41d7a7d9609c734c8704fe68af5e74217895f861
Kausar Hasan <kausarhsn92@gmail.com>
2024-05-10 21:46:10 +0600

    Enrollment Change

--- src/pages/Enrollments.jsx
+++ src/pages/Enrollments.jsx
@@ -13,11 +13,14 @@ const Enrollments = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
   const userToken = useSelector(selectUserToken);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [payStatus, setPayStatus] = useState('')
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["enrollments"],
-    queryFn: () => getData(userToken, "enrollments"),
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["enrollments", currentPage],
+    queryFn: () => getData(userToken, `enrollments?page=${currentPage}&limit=5${payStatus?`&paymentStatus=${payStatus}`:""}`),
   });
+
 
   const options = [
     { value: "pending", label: "Pending" },
@@ -35,15 +38,30 @@ const Enrollments = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  }
 
   if (isLoading) return <Loader />;
-  const enrollmentsdata = data?.data;
+  const enrollmentsdata = data?.data.enrollments;
   return (
     <div>
       <h2 className="text-2xl mt-10">All the Enrollments</h2>
       {enrollmentsdata.map((item, index) => (
         <EnrollmentCard data={item} openModal={openModal} key={index} />
       ))}
+
+      <div className="flex justify-center mt-10">
+        <div>
+        {currentPage > 1 && (
+          <button onClick={() => handlePageChange(currentPage - 1)}>Previous</button>
+        )}
+        <span>Page {currentPage}</span>
+        {data?.data.totalPages > currentPage && (
+          <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+        )}
+        </div>
+      </div>
 
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <SelectField
